b8628758930294ae15fb3975620eaa489172137b
tannerb9 <tanner-brainard@hotmail.com>
2021-03-16 19:33:21 -0500

    fix: removed duplicate pythonPath code so interpreter could be selected

--- hello/views.py
+++ hello/views.py
@@ -13,7 +13,6 @@ class SchoolsViewSet(viewsets.ModelViewSet):
     serializer_class = SchoolsSerializer
     # permission_classes = [permissions.IsAuthenticated]
 
-
 class PreAssessmentViewset(viewsets.ModelViewSet):
     queryset = Preassessment.objects.all()
     serializer_class = PreAssessmentSerializer
@@ -27,7 +26,7 @@ answerKey = {
     'q01_answer': 'b', 'q02_answer': 'b', 'q03_answer': 'a', 'q04_answer': 'c',
     'q05_answer': 'd', 'q06_answer': 'b', 'q07_answer': 'd', 'q08_answer': 'b',
     'q09_answer': 'd', 'q10_answer': 'a', 'q11_answer': '62', 'q12_answer': '20',
-    'q13_answer': '42', 'q14_answer': 'Macy\'s', 'q15_answer': 'Three and 75/100', 'q16_answer': '',
-    'q17_answer': '3.75', 'q18_answer': '21.25', 'q19_answer': 'Journeys', 'q20_answer': '5.00',
-    'q21_answer': '5.00', 'q22_answer': '16.25'
+    'q13_answer': '42', 'q14_answer': 'Macy\'s', 'q15_answer': 'Three and 75/100',
+    'q16_answer': '', 'q17_answer': '3.75', 'q18_answer': '21.25', 'q19_answer': 'Journeys',
+    'q20_answer': '5.00', 'q21_answer': '5.00', 'q22_answer': '16.25'
 }
