03c2a12e4c482a648dc07666746f6036b2283bbb
tannerb9 <tanner-brainard@hotmail.com>
2021-03-16 23:40:23 -0500

    refactor: consolidated Pre and PostAssessment models to Assessment and added a 'pretest' boolean field to indicate if it is a pre or post test. added a new serializer and viewset(Assessment)

--- hello/urls.py
+++ hello/urls.py
@@ -3,9 +3,10 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'schools', views.SchoolsViewSet)
-router.register(r'preassessment', views.PreAssessmentViewset)
-router.register(r'postassessment', views.PostAssessmentViewset)
+router.register(r'schools', views.SchoolViewSet)
+router.register(r'assessments', views.AssessmentViewset)
+# router.register(r'preassessment', views.PreAssessmentViewset)
+# router.register(r'postassessment', views.PostAssessmentViewset)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
--- hello/views.py
+++ hello/views.py
@@ -3,23 +3,123 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from .serializers import *
 from .models import *
+from rest_framework.response import Response
 
 
-class SchoolsViewSet(viewsets.ModelViewSet):
+
+class SchoolViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    queryset = Schools.objects.all()
-    serializer_class = SchoolsSerializer
+    queryset = School.objects.all()
+    serializer_class = SchoolSerializer
     # permission_classes = [permissions.IsAuthenticated]
 
-class PreAssessmentViewset(viewsets.ModelViewSet):
-    queryset = Preassessment.objects.all()
-    serializer_class = PreAssessmentSerializer
 
-class PostAssessmentViewset(viewsets.ModelViewSet):
-    queryset = Postassessment.objects.all()
-    serializer_class = PostAssessmentSerializer
+class AssessmentViewset(viewsets.ViewSet):
+
+    queryset = Assessment.objects.all()
+
+    def create(self, request):
+
+        assessment = Assessment()
+        assessment.last_name = request.data['last_name']
+        assessment.teacher = request.data['teacher']
+        assessment.class_grade = request.data['class_grade']
+        assessment.birth_date = request.data['birth_date']
+        assessment.school = request.data['school']
+        assessment.previous_participation = request.data['previous_participation']
+        assessment.q01_answer = request.data['q01_answer']
+        assessment.q02_answer = request.data['q02_answer']
+        assessment.q03_answer = request.data['q03_answer']
+        assessment.q04_answer = request.data['q04_answer']
+        assessment.q05_answer = request.data['q05_answer']
+        assessment.q06_answer = request.data['q06_answer']
+        assessment.q07_answer = request.data['q07_answer']
+        assessment.q08_answer = request.data['q08_answer']
+        assessment.q09_answer = request.data['q09_answer']
+        assessment.q10_answer = request.data['q10_answer']
+        assessment.q11_answer = request.data['q11_answer']
+        assessment.q12_answer = request.data['q12_answer']
+        assessment.q13_answer = request.data['q13_answer']
+        assessment.q14_answer = request.data['q14_answer']
+        assessment.q15_answer = request.data['q15_answer']
+        assessment.q16_answer = request.data['q16_answer']
+        assessment.q17_answer = request.data['q17_answer']
+        assessment.q18_answer = request.data['q18_answer']
+        assessment.q19_answer = request.data['q19_answer']
+        assessment.q20_answer = request.data['q20_answer']
+        assessment.q21_answer = request.data['q21_answer']
+        assessment.q22_answer = request.data['q22_answer']
+        assessment.q23_answer = request.data['q23_answer']
+        assessment.q24_answer = request.data['q24_answer']
+        assessment.q25_answer = request.data['q25_answer']
+        assessment.q26_answer = request.data['q26_answer']
+        assessment.q27_answer = request.data['q27_answer']
+        assessment.q28_answer = request.data['q28_answer']
+        assessment.q29_answer = request.data['q29_answer']
+        assessment.q30_answer = request.data['q30_answer']
+        assessment.q31_answer = request.data['q31_answer']
+        assessment.q32_answer = request.data['q32_answer']
+        assessment.q33_answer = request.data['q33_answer']
+        assessment.q34_answer = request.data['q34_answer']
+        assessment.q35_answer = request.data['q35_answer']
+
+
+        serializer = AssessmentSerializer(assessment, context={'request': request})
+        return Response(serializer.data)
+
+# class PreAssessmentViewset(viewsets.ViewSet):
+#     queryset = Preassessment.objects.all()
+#     # serializer_class = PreAssessmentSerializer
+
+#     def create(self, request):
+#         preassessment = Preassessment()
+#         preassessment.last_name = request.data['last_name']
+#         preassessment.teacher = request.data['teacher']
+#         preassessment.class_grade = request.data['class_grade']
+#         preassessment.birth_date = request.data['birth_date']
+#         preassessment.school = request.data['school']
+#         preassessment.previous_participation = request.data['previous_participation']
+#         preassessment.q01_answer = request.data['q01_answer']
+#         preassessment.q02_answer = request.data['q02_answer']
+#         preassessment.q03_answer = request.data['q03_answer']
+#         preassessment.q04_answer = request.data['q04_answer']
+#         preassessment.q05_answer = request.data['q05_answer']
+#         preassessment.q06_answer = request.data['q06_answer']
+#         preassessment.q07_answer = request.data['q07_answer']
+#         preassessment.q08_answer = request.data['q08_answer']
+#         preassessment.q09_answer = request.data['q09_answer']
+#         preassessment.q10_answer = request.data['q10_answer']
+#         preassessment.q11_answer = request.data['q11_answer']
+#         preassessment.q12_answer = request.data['q12_answer']
+#         preassessment.q13_answer = request.data['q13_answer']
+#         preassessment.q14_answer = request.data['q14_answer']
+#         preassessment.q15_answer = request.data['q15_answer']
+#         preassessment.q16_answer = request.data['q16_answer']
+#         preassessment.q17_answer = request.data['q17_answer']
+#         preassessment.q18_answer = request.data['q18_answer']
+#         preassessment.q19_answer = request.data['q19_answer']
+#         preassessment.q20_answer = request.data['q20_answer']
+#         preassessment.q21_answer = request.data['q21_answer']
+#         preassessment.q22_answer = request.data['q22_answer']
+#         preassessment.q23_answer = request.data['q23_answer']
+#         preassessment.q24_answer = request.data['q24_answer']
+#         preassessment.q25_answer = request.data['q25_answer']
+#         preassessment.q26_answer = request.data['q26_answer']
+#         preassessment.q27_answer = request.data['q27_answer']
+#         preassessment.q28_answer = request.data['q28_answer']
+#         preassessment.q29_answer = request.data['q29_answer']
+#         preassessment.q30_answer = request.data['q30_answer']
+#         preassessment.q31_answer = request.data['q31_answer']
+#         preassessment.q32_answer = request.data['q32_answer']
+       
+#         serializer = PressessmentSerializer(preassessment, context={'request': request})
+#         return Response(serializer.data)
+
+# class PostAssessmentViewset(viewsets.ViewSet):
+#     queryset = Postassessment.objects.all()
+#     serializer_class = PostAssessmentSerializer
 
 
 answerKey = {
