010c5203019eb15472fdd57742349227a7371505
tannerb9 <tanner-brainard@hotmail.com>
2021-04-22 11:43:47 -0500

    fix: added https:// to correct CORS Whitelist errors

--- biztown_server/django_hello/settings.py
+++ biztown_server/django_hello/settings.py
@@ -69,8 +69,8 @@ MIDDLEWARE = [
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://ja-web.azurewebsites.net/',
-    '20.40.202.15'
+    'https://ja-web.azurewebsites.net',
+    'https://20.40.202.15'
 )
 
 ROOT_URLCONF = 'django_hello.urls'
