2b53495125be2bcdde85a6c422b2cc4642d03348
tannerb9 <tanner-brainard@hotmail.com>
2021-04-22 23:53:58 -0500

    chore: removed zombie code from settings.py

--- biztown_server/django_hello/settings.py
+++ biztown_server/django_hello/settings.py
@@ -67,10 +67,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_WHITELIST = (
-    'http://localhost:3000',
     'http://127.0.0.1:3000',
     'https://ja-web.azurewebsites.net',
-    'https://20.40.202.15'
 )
 
 ROOT_URLCONF = 'django_hello.urls'
