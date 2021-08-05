af5226d48d582f435cc9498f320ae0c157bc89ac
tannerb9 <tanner-brainard@hotmail.com>
2021-08-05 17:01:21 -0400

    feat: created CompletionPage component

---
+++ biztown/src/components/CompletionPage.js
@@ -0,0 +1,18 @@
+import headerLogo from "./../JAInspiringHeader.png";
+
+const CompletionPage = () => {
+  return (
+    <div
+      style={{
+        maxWidth: "900px",
+        marginLeft: "auto",
+        marginRight: "auto",
+      }}
+    >
+      <img src={headerLogo} alt="Girl in a jacket" width="100%"></img>
+      <h2>Thank you for participating in our BizTown program!</h2>
+    </div>
+  );
+};
+
+export default CompletionPage;
